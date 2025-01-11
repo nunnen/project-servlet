92c46e471527beb94b963f78a9718e82c640eede
Niyaz <niazsajdullin@gmail.com>
2025-01-12 00:11:40 +0500

    add Signs set to LogicServlet

--- src/main/java/com/tictactoe/LogicServlet.java
+++ src/main/java/com/tictactoe/LogicServlet.java
@@ -5,14 +5,27 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet(name = "LogicServlet", urlPatterns = "/logic")
 public class LogicServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+
+        Field field = extractField(session);
+
         int index = getSelectedIndex(req);
+
+        List<Sign> data = field.getFieldData();
+        data.set(index, Sign.CROSS);
+
+        session.setAttribute("data", data);
+        session.setAttribute("field", field);
+
         resp.sendRedirect("/index.jsp");
     }
 
@@ -20,5 +33,14 @@ public class LogicServlet extends HttpServlet {
         String onClick = request.getParameter("click");
         boolean isNumeric = onClick.matches("[0-9]*");
         return isNumeric ? Integer.parseInt(onClick) : 0;
+    }
+
+    private Field extractField(HttpSession session) {
+        Object fieldAttribute = session.getAttribute("field");
+        if (Field.class != fieldAttribute.getClass()) {
+            session.invalidate();
+            throw new RuntimeException("Invalid field attribute");
+        }
+        return (Field) fieldAttribute;
     }
 }
