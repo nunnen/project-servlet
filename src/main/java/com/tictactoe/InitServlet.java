a7c0461c8d24f88781460754f6e090de5bd1660e
Niyaz <niazsajdullin@gmail.com>
2025-01-11 23:57:17 +0500

    add InitServlet

---
+++ src/main/java/com/tictactoe/InitServlet.java
@@ -0,0 +1,34 @@
+package com.tictactoe;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+@WebServlet(name = "InitServlet", urlPatterns = "/start")
+public class InitServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // Создание новой сессии
+        HttpSession session = req.getSession();
+
+        // Создание игрового поля
+        Field field = new Field();
+        Map<Integer, Sign> fieldData = field.getField();
+
+        // Получение списка значений поля
+        List<Sign> data = field.getFieldData();
+
+        // Добавление в сессию параметров поля
+        session.setAttribute("field", field);
+        session.setAttribute("data", data);
+
+        req.getRequestDispatcher("/index.jsp").forward(req, resp);
+    }
+}
