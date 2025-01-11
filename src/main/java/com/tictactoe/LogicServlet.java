d6a84a6236e811806a43e6d4c16fe03c04c92a46
Niyaz <niazsajdullin@gmail.com>
2025-01-11 23:48:41 +0500

    add LogicServlet

--- src/main/java/com/tictactoe/LogicServlet.java
+++ src/main/java/com/tictactoe/LogicServlet.java
@@ -1,4 +1,24 @@
 package com.tictactoe;
 
-public class LogicServlet {
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "LogicServlet", urlPatterns = "/logic")
+public class LogicServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        int index = getSelectedIndex(req);
+        resp.sendRedirect("/index.jsp");
+    }
+
+    private int getSelectedIndex(HttpServletRequest request) {
+        String onClick = request.getParameter("click");
+        boolean isNumeric = onClick.matches("[0-9]*");
+        return isNumeric ? Integer.parseInt(onClick) : 0;
+    }
 }
