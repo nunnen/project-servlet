ed3b04b1daf3a291ae8d9d06c6d3be0815365247
Niyaz <niazsajdullin@gmail.com>
2025-01-13 17:50:54 +0500

    add AI (nought signs) + check win condition

--- src/main/java/com/tictactoe/LogicServlet.java
+++ src/main/java/com/tictactoe/LogicServlet.java
@@ -18,12 +18,38 @@ public class LogicServlet extends HttpServlet {
 
         Field field = extractField(session);
 
+        // получаем номер клетки
         int index = getSelectedIndex(req);
+        // получаем знак в текущей клетке
+        Sign currentSign = field.getFieldData().get(index);
 
+        // если клетка не пустая, то возвращаем пользователя на текущую страницу
+        // без изменений
+        if (currentSign != Sign.EMPTY) {
+            req.getRequestDispatcher("logic.jsp").forward(req, resp);
+            return;
+        }
+
+        // иначе ставим крестик в клетке
         field.getField().put(index, Sign.CROSS);
+        if (checkWin(session, resp, field)) {
+            return;
+        }
 
+        // получаем индекс поля с пустой клеткой
+        // и ставим туда ноль (AI)
+        int emptyFieldIndex = field.getEmptyFieldIndex();
+        if (emptyFieldIndex >= 0) {
+            field.getField().put(emptyFieldIndex, Sign.NOUGHT);
+            if (checkWin(session, resp, field)) {
+                return;
+            }
+        }
+
+        // список знаков по индексам полей
         List<Sign> data = field.getFieldData();
 
+        // отправляем данные в сессию
         session.setAttribute("data", data);
         session.setAttribute("field", field);
 
@@ -43,5 +69,21 @@ public class LogicServlet extends HttpServlet {
             throw new RuntimeException("Invalid field attribute");
         }
         return (Field) fieldAttribute;
+    }
+
+    private boolean checkWin(HttpSession session, HttpServletResponse response, Field field) throws IOException {
+        Sign winner = field.checkWin();
+        if (winner == Sign.CROSS || winner == Sign.NOUGHT) {
+            // добавляем флаг, что есть победитель
+            session.setAttribute("winner", winner);
+            // получаем список значений поля
+            List<Sign> data = field.getFieldData();
+            // обновляем этот список и отправляем на сессию
+            session.setAttribute("data", data);
+            // шлем редирект
+            response.sendRedirect("/index.jsp");
+            return true;
+        }
+        return false;
     }
 }
